89cba23e1baefb42ce66e35ad43cce06f2be3210
barasher <arnaud.barasher@gmail.com>
2020-10-16 22:04:39 +0200

    Refactors "diff" function

--- internal/diff.go
+++ internal/diff.go
@@ -17,10 +17,13 @@ func Diff(ctx context.Context, inChan chan Entry) {
 				return
 			}
 			for _, v := range coll {
-				diff := cur.Hash ^ v.Hash
-				fmt.Printf("%v ^ %v: %v\n", cur.Path, v.Path, bits.OnesCount64(diff))
+				fmt.Printf("%v ^ %v: %v\n", cur.Path, v.Path, diff(cur, v))
 			}
 			coll = append(coll, cur)
 		}
 	}
+}
+
+func diff(e1, e2 Entry) int {
+	return bits.OnesCount64(e1.Hash ^ e2.Hash)
 }
